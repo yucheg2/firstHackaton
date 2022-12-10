f124da98c4ad7a4c18e2268d4ea2c90399b595d8
Igor Zhigalov <zhigalovzenit112233@gmail.com>
2022-12-10 18:38:26 +0300

    Igor add context menu + backgroundModule

--- src/app.js
+++ src/app.js
@@ -1,6 +1,36 @@
 import './styles.css'
 import { ContextMenu } from './menu.js'
+// import { GameDrumModule } from './modules/drum-game.module'
+import { BackgroundModule } from './modules/background.module'
 
-const contextMenu = new ContextMenu()
+const contextMenu = new ContextMenu('#menu')
 
-contextMenu.startMenu()
+let arrayModules = [];
+
+const firstModule = new BackgroundModule('changeBack','Изменить задний фон');
+const secondModule = new BackgroundModule('changeColor','Изменить цвет');
+
+arrayModules.push(firstModule)
+arrayModules.push(secondModule)
+
+arrayModules.forEach ((item) => {
+    contextMenu.add(item)
+})
+
+
+document.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (target.dataset.type == 'changeColor') {
+        firstModule.trigger()
+        contextMenu.close()
+    }
+})
+
+
+// contextMenu.startMenu()
+
+
+// const game = new GameDrumModule('game','drum')
+// game.trigger()
+// game.toHTML()
--- src/menu.js
+++ src/menu.js
@@ -1,38 +1,60 @@
 import {Menu} from './core/menu'
 
-function adElementToMenu(name, menu, id) {
-    const liItem = document.createElement('li')
-    liItem.className = 'menu-item'
-    liItem.setAttribute('data-id', id)
-    liItem.textContent = name
-    menu.append(liItem)
-}
+// function adElementToMenu(name, menu, id) {
+//     const liItem = document.createElement('li')
+//     liItem.className = 'menu-item'
+//     liItem.setAttribute('data-id', id)
+//     liItem.textContent = name
+//     menu.append(liItem)
+// }
 
 export class ContextMenu extends Menu {
-    startMenu() {
-        const menu = document.querySelector('#menu')
-        adElementToMenu('первая игра', menu, 1)
-        
-        menu.append('')
+    constructor(selector) {
+        super(selector)
         document.addEventListener('contextmenu', (event) => {
-            event.preventDefault()
-            menu.style.top = `${event.clientY}px`
-            menu.style.left = `${event.clientX}px`
-            menu.classList.add('open')
+            event.preventDefault();
+            // console.log(event.clientY, event.clientX);
+            this.open(event.clientY, event.clientX)
         })
-        document.addEventListener('click', (event) => {
-            if (event.button !== 2) {
-                menu.classList.remove('open')
-            }
-        }, false)        
-        menu.addEventListener('click', (event) => {         
-            const {target} = event
-            event.stopPropagation()
-            if (target.getAttribute('data-id') == 1) {
-                console.log(target.getAttribute('data-id'))
-                console.log('запуск первой игры')
-                //reaction.run()
-            }
-        }, false)        
+    }
+    // startMenu() {
+    //     // const menu = document.querySelector('#menu')
+    //     adElementToMenu('первая игра', menu, 1)
+    //     console.log(this.el);
+    //     menu.append('')
+    //     document.addEventListener('contextmenu', (event) => {
+    //         event.preventDefault()
+    //         menu.style.top = `${event.clientY}px`
+    //         menu.style.left = `${event.clientX}px`
+    //         menu.classList.add('open')
+    //     })
+    //     document.addEventListener('click', (event) => {
+    //         if (event.button !== 2) {
+    //             menu.classList.remove('open')
+    //         }
+    //     }, false)        
+    //     menu.addEventListener('click', (event) => {         
+    //         const {target} = event
+    //         event.stopPropagation()
+    //         if (target.getAttribute('data-id') == 1) {
+    //             console.log(target.getAttribute('data-id'))
+    //             console.log('запуск первой игры')
+    //             //reaction.run()
+    //         }
+    //     }, false)        
+    // }
+    open (top, left) {
+        this.el.style.top = `${top}px`
+        this.el.style.left = `${left}px`
+        this.el.classList.add('open')
+    }
+
+    add (m) {
+        console.log(m);
+        this.el.insertAdjacentHTML('beforeend', m.toHTML())
+    }
+
+    close () {
+        this.el.classList.remove('open')
     }
 }
